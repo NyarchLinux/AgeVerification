13909225bb5e33a518833eb87cfa4e1103047467
Francesco Caracciolo <67018178+FrancescoCaracciolo@users.noreply.github.com>
2026-07-17 17:52:37 +0200

    Update manifest

---
+++ install.sh
@@ -0,0 +1,9 @@
+
+#!/bin/bash
+APPID="moe.nyarchlinux.ageverification"
+BUNDLENAME="ageverification.flatpak"
+flatpak-builder --install --user --force-clean flatpak-app "$APPID".json
+
+if [ "$1" = "bundle" ]; then
+	flatpak build-bundle ~/.local/share/flatpak/repo "$BUNDLENAME" "$APPID"
+fi
